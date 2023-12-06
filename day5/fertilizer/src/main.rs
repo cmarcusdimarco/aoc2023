2cb8a3315ceb6a902b7c1a1b7387448f73090ed7
C. Marcus DiMarco <79232853+cmarcusdimarco@users.noreply.github.com>
2023-12-05 20:25:17 -0500

    Successful part 1

--- day5/fertilizer/src/main.rs
+++ day5/fertilizer/src/main.rs
@@ -4,36 +4,39 @@ use std::fs;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct RangeMap {
-    source: u32,
-    destination: u32,
-    range: u32
+    source: u64,
+    destination: u64,
+    range: u64,
 }
 
 // Shorthand to construct a RangeMap from an ordered tuple
 impl RangeMap {
-    fn new(values: (u32, u32, u32)) -> Self {
+    fn new(values: (u64, u64, u64)) -> Self {
         Self {
-            source: values.0,
-            destination: values.1,
-            range: values.2
+            source: values.1,
+            destination: values.0,
+            range: values.2,
         }
     }
 }
 
-fn parse_seeds(s: &str) -> Vec<u32> {
-    let mut seeds: Vec<u32> = Vec::new();
+fn parse_seeds(s: &str) -> Vec<u64> {
+    let mut seeds: Vec<u64> = Vec::new();
 
     match s.split_once(": ") {
         Some((a, b)) if a == "seeds" => {
             for value in b.split_whitespace() {
-                seeds.push(value.parse::<u32>().expect("Seed values should all be numbers."))
+                seeds.push(
+                    value
+                        .parse::<u64>()
+                        .expect("Seed values should all be numbers."),
+                )
             }
 
             seeds
-        },
-        _ => panic!("Not a valid seed string")
+        }
+        _ => panic!("Not a valid seed string"),
     }
-    
 }
 
 fn parse_maps(s: &str) -> Vec<Vec<RangeMap>> {
@@ -54,7 +57,7 @@ fn parse_maps(s: &str) -> Vec<Vec<RangeMap>> {
         water_light_map,
         light_temperature_map,
         temperature_humidity_map,
-        humidity_location_map
+        humidity_location_map,
     ];
 
     let mut map_iterator = 0;
@@ -67,10 +70,10 @@ fn parse_maps(s: &str) -> Vec<Vec<RangeMap>> {
             l if l.starts_with(|c: char| c.is_ascii_digit()) => {
                 let values = l.split(" ");
 
-                let values: Vec<u32> = values.map(|val| val.parse::<u32>().unwrap()).collect();
-                
+                let values: Vec<u64> = values.map(|val| val.parse::<u64>().unwrap()).collect();
+
                 maps[map_iterator].push(RangeMap::new((values[0], values[1], values[2])));
-            },
+            }
             _ => continue,
         }
     }
@@ -78,10 +81,10 @@ fn parse_maps(s: &str) -> Vec<Vec<RangeMap>> {
     maps
 }
 
-fn find_lowest_location_value(file: &str) -> u32 {
+fn find_lowest_location_value(file: &str) -> u64 {
     // Read input
     let input = fs::read_to_string(file).expect("File not found.");
-    let mut seeds;
+    let seeds;
     let mut maps;
 
     // Get the seeds and maps
@@ -89,34 +92,48 @@ fn find_lowest_location_value(file: &str) -> u32 {
         Some((a, b)) => {
             seeds = parse_seeds(a);
             maps = parse_maps(b);
-        },
-        _ => panic!("Input was not formatted as expected - please check file contents and try again."),
+        }
+        _ => panic!(
+            "Input was not formatted as expected - please check file contents and try again."
+        ),
     }
 
     // Sort the maps by the source input to speed up the search process
-    for map in maps {
+    for map in &mut maps {
         map.sort()
     }
 
-    let mut minimum_location_value = std::u32::MAX;
+    let mut minimum_location_value = std::u64::MAX;
 
     for seed in seeds {
         let mut value = seed;
-        for map in maps {
+        for map in &maps {
             'current_range_map_loop: for range_map in map {
-                // Since the RangeMaps are sorted, we can match
+                // Since the RangeMaps are sorted, we can break out early if we
+                // a) find a match for our target range, or
+                // b) find a source greater than our value
                 match range_map {
-                    rm if (rm.source..rm.source + rm.range).contains(value) => value = rm.destination + (value - rm.source),
+                    rm if (rm.source..rm.source + rm.range).contains(&value) => {
+                        value = rm.destination + (value - rm.source);
+                        break 'current_range_map_loop;
+                    }
                     rm if rm.source > value => break 'current_range_map_loop,
                     _ => (),
                 }
             }
         }
+
+        minimum_location_value = std::cmp::min(minimum_location_value, value);
     }
+
+    minimum_location_value
 }
 
 fn main() {
-    println!("The lowest location value is {:?}", find_lowest_location_value("input.txt"));
+    println!(
+        "The lowest location value is {:?}",
+        find_lowest_location_value("input.txt")
+    );
 }
 
 #[cfg(test)]
@@ -137,10 +154,23 @@ mod tests {
     fn parses_maps_as_expected() {
         let expected = vec![
             vec![RangeMap::new((50, 98, 2)), RangeMap::new((52, 50, 48))],
-            vec![RangeMap::new((0, 15, 37)), RangeMap::new((37, 52, 2)), RangeMap::new((39, 0, 15))],
-            vec![RangeMap::new((49, 53, 8)), RangeMap::new((0, 11, 42)), RangeMap::new((42, 0, 7)), RangeMap::new((57, 7, 4))],
+            vec![
+                RangeMap::new((0, 15, 37)),
+                RangeMap::new((37, 52, 2)),
+                RangeMap::new((39, 0, 15)),
+            ],
+            vec![
+                RangeMap::new((49, 53, 8)),
+                RangeMap::new((0, 11, 42)),
+                RangeMap::new((42, 0, 7)),
+                RangeMap::new((57, 7, 4)),
+            ],
             vec![RangeMap::new((88, 18, 7)), RangeMap::new((18, 25, 70))],
-            vec![RangeMap::new((45, 77, 23)), RangeMap::new((81, 45, 19)), RangeMap::new((68, 64, 13))],
+            vec![
+                RangeMap::new((45, 77, 23)),
+                RangeMap::new((81, 45, 19)),
+                RangeMap::new((68, 64, 13)),
+            ],
             vec![RangeMap::new((0, 69, 1)), RangeMap::new((1, 0, 69))],
             vec![RangeMap::new((60, 56, 37)), RangeMap::new((56, 93, 4))],
         ];
