d7be0e9184bb2ecad4d43cea97c206c27a9f3e4f
C. Marcus DiMarco <79232853+cmarcusdimarco@users.noreply.github.com>
2023-12-14 20:57:35 -0500

    Successful run

--- day14/parabolic-reflector-dish/src/main.rs
+++ day14/parabolic-reflector-dish/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 fn parse_platform(path: &str) -> Vec<Vec<char>> {
     let input = fs::read_to_string(path).expect("Error reading file at specified path");
     let mut platform: Vec<Vec<char>> = Vec::new();
-    
+
     for (index, line) in input.lines().enumerate() {
         platform.push(Vec::new());
 
@@ -22,12 +22,58 @@ fn roll_north(platform: &mut Vec<Vec<char>>) {
     // n + 1 through x - 1, where x is the index of the last # seen (or the
     // max value, if a # hasn't been seen yet.)
     for column in 0..platform[0].len() {
-        
+        let mut counter = 0;
+        let mut previous_cube = platform.len();
+
+        for (i, row) in platform.clone().iter().enumerate().rev() {
+            match row[column] {
+                'O' => counter += 1,
+                '#' => {
+                    for index in i + 1..previous_cube {
+                        match counter {
+                            x if x > 0 => {
+                                platform[index][column] = 'O';
+                                counter -= 1;
+                            }
+                            _ => platform[index][column] = '.',
+                        }
+                    }
+
+                    previous_cube = i;
+                }
+                _ => (),
+            }
+        }
+
+        // If we have reached this point with a non-zero counter, we need to push to the top.
+        if counter > 0 {
+            for index in 0..previous_cube {
+                match counter {
+                    x if x > 0 => {
+                        platform[index][column] = 'O';
+                        counter -= 1;
+                    }
+                    _ => platform[index][column] = '.',
+                }
+            }
+        }
     }
 }
 
+fn calculate_load(platform: &Vec<Vec<char>>) -> usize {
+    platform.iter().enumerate().fold(0, |acc, (index, row)| {
+        acc + row.iter().filter(|c| **c == 'O').count() * (platform.len() - index)
+    })
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut platform = parse_platform("input.txt");
+    roll_north(&mut platform);
+
+    println!(
+        "The sum of the load caused by the rounded rocks is: {:?}",
+        calculate_load(&platform)
+    );
 }
 
 #[cfg(test)]
@@ -37,18 +83,47 @@ mod tests {
     #[test]
     fn parses_platform() {
         let expected = vec![
-            vec!['O','.','.','.','.','#','.','.','.','.'],
-            vec!['O','.','O','O','#','.','.','.','.','#'],
-            vec!['.','.','.','.','.','#','#','.','.','.'],
-            vec!['O','O','.','#','O','.','.','.','.','O'],
-            vec!['.','O','.','.','.','.','.','O','#','.'],
-            vec!['O','.','#','.','.','O','.','#','.','#'],
-            vec!['.','.','O','.','.','#','O','.','.','O'],
-            vec!['.','.','.','.','.','.','.','O','.','.'],
-            vec!['#','.','.','.','.','#','#','#','.','.'],
-            vec!['#','O','O','.','.','#','.','.','.','.'],
+            vec!['O', '.', '.', '.', '.', '#', '.', '.', '.', '.'],
+            vec!['O', '.', 'O', 'O', '#', '.', '.', '.', '.', '#'],
+            vec!['.', '.', '.', '.', '.', '#', '#', '.', '.', '.'],
+            vec!['O', 'O', '.', '#', 'O', '.', '.', '.', '.', 'O'],
+            vec!['.', 'O', '.', '.', '.', '.', '.', 'O', '#', '.'],
+            vec!['O', '.', '#', '.', '.', 'O', '.', '#', '.', '#'],
+            vec!['.', '.', 'O', '.', '.', '#', 'O', '.', '.', 'O'],
+            vec!['.', '.', '.', '.', '.', '.', '.', 'O', '.', '.'],
+            vec!['#', '.', '.', '.', '.', '#', '#', '#', '.', '.'],
+            vec!['#', 'O', 'O', '.', '.', '#', '.', '.', '.', '.'],
         ];
 
         assert_eq!(expected, parse_platform("test.txt"))
+    }
+
+    #[test]
+    fn rolls_north() {
+        let expected = vec![
+            vec!['O', 'O', 'O', 'O', '.', '#', '.', 'O', '.', '.'],
+            vec!['O', 'O', '.', '.', '#', '.', '.', '.', '.', '#'],
+            vec!['O', 'O', '.', '.', 'O', '#', '#', '.', '.', 'O'],
+            vec!['O', '.', '.', '#', '.', 'O', 'O', '.', '.', '.'],
+            vec!['.', '.', '.', '.', '.', '.', '.', '.', '#', '.'],
+            vec!['.', '.', '#', '.', '.', '.', '.', '#', '.', '#'],
+            vec!['.', '.', 'O', '.', '.', '#', '.', 'O', '.', 'O'],
+            vec!['.', '.', 'O', '.', '.', '.', '.', '.', '.', '.'],
+            vec!['#', '.', '.', '.', '.', '#', '#', '#', '.', '.'],
+            vec!['#', '.', '.', '.', '.', '#', '.', '.', '.', '.'],
+        ];
+
+        let mut platform = parse_platform("test.txt");
+        roll_north(&mut platform);
+
+        assert_eq!(expected, platform)
+    }
+
+    #[test]
+    fn calculates_load() {
+        let mut platform = parse_platform("test.txt");
+        roll_north(&mut platform);
+
+        assert_eq!(136usize, calculate_load(&platform))
     }
 }
